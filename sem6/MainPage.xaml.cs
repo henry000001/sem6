1df397e74b0be890b56ce21500579818509bab81
master <master@SMLAB408PC03_22>
2022-05-14 11:40:00 -0500

    Agregar archivos de proyecto.

---
+++ sem6/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace sem6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btninsertar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+                parametros.Add("codigo", txtcodigo.Text);
+                parametros.Add("nombre", txtnombre.Text);
+                parametros.Add("apellido", txtapellido.Text);
+                parametros.Add("edad", txtedad.Text);
+
+                cliente.UploadValues("http://192.168.22.24/moviles/post.php", "POST", parametros);
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+            }
+
+        }
+
+        private void btnregresar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ sem6/get.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace sem6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class get : ContentPage
+    {
+        public get()
+        {
+            InitializeComponent();
+            
+        
+
+        }
+
+        private async void btnget_Clicked(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
